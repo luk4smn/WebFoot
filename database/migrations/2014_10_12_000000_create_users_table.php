88283155804b99720168a5fdef6067bd0c1e3954
lucas <lucas.mc.martins@gmail.com>
2017-11-26 16:14:27 -0300

    Fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('time_id');
+            $table->string('time_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
