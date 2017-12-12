b1e426991a4b1764268ed27b0c3fbc269d59e808
lucas <lucas.mc.martins@gmail.com>
2017-12-12 00:16:39 -0300

    + fix

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,7 +26,17 @@ class HomeController extends Controller
             return view('time.selectTeam', compact('times'));
         }
 
-        return view('home');
+        $proximo_jogo = Partida::where('time_mandante_id',auth()->user()->time_id)
+            ->orWhereRaw('time_visitante_id',auth()->user()->time_id)
+            ->where('placar_mandante',null)
+            ->where('placar_visitante', null)
+            ->orderBy('partidas.id')
+            ->first();
+
+        $mandante = Time::findOrFail($proximo_jogo->time_mandante_id);
+        $visitante = Time::findOrFail($proximo_jogo->time_visitante_id);
+
+        return view('home',compact('proximo_jogo','mandante','visitante'));
     }
 
     public function setMyTeam(Request $request){
