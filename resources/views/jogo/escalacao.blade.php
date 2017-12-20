cce97454484ee2c4d2cd8e88cdc41be1d6861ef9
lucas <lucas.mc.martins@gmail.com>
2017-12-20 16:33:53 -0300

    teste

--- resources/views/jogo/escalacao.blade.php
+++ resources/views/jogo/escalacao.blade.php
@@ -1,7 +1,30 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: lucas
- * Date: 18/12/17
- * Time: 21:54
- */
+@extends('templates.template')
+
+@section('content')
+    <div class="card mb-3">
+    <div class="card-header">
+        <i class="fa fa-table"></i> Proxima Partida :
+    </div>
+    <div class="card-body" style="width:100%;padding:8px;background:url('img/campo.png')">
+        <div class="table-responsive">
+            <table class="table table-dark" width="100%" cellspacing="0">
+                <thead>
+                <tr align="center">
+                    <th>Casa</th>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th>Fora</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr align="center">
+
+                </tr>
+            </table>
+        </div>
+
+    </div>
+    </div>
+
+@endsection
